7d0f96a237b66b1bf0ac462f74f63c5b87398016
satya <satya@SoftEr.mshome.net>
2022-08-18 16:53:16 +0530

    uploading pattern project

---
+++ PatternProject/src/NumberPattern.java
@@ -0,0 +1,30 @@
+
+
+public class NumberPattern
+{
+	/*
+	 * This program is for printing a number pattern shown below:
+	 * 1
+	 * 121
+	 * 12321
+	 * 1234321
+	 */
+    public static void main(String[] args)
+    {
+        
+        System.out.println("** Printing the pattern... **");
+
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for (int k = i - 1; k >= 1; k--)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
